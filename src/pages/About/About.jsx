a8b6e3bc91e4594143e8c70467aaca5fc4933766
iaymergen <iaymergen@hotmail.com>
2023-12-18 01:26:20 +0300

    magazine

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -31,6 +31,10 @@ const About = () => {
         <div className="title"><h1>Neler Yapıyoruz?</h1> <hr /></div>
         <div className="content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
       </div>
+      <div className='left-card'>
+        <div className="title"><hr /><h1>Fotoğraflarımız</h1> </div>
+        
+      </div>
       
       
       <PhotoGallery photos={photos} />
--- src/pages/Magazine/Magazine.jsx
+++ src/pages/Magazine/Magazine.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import "./magazine.css"
+import EventGallery from '../../components/EventGallery/EventGallery'
 
 const Magazine = () => {
+
+  const dummyMagazineData = [
+    {
+      id: 1,
+      title: "Exciting Events in the Tech World",
+      mainPhoto: "http://localhost:3000/img/photos/deneme.jpg",
+      url: "https://example.com/magazine1",
+    },
+    {
+      id: 2,
+      title: "Innovation Unleashed: The Latest in Science",
+      mainPhoto: "http://localhost:3000/img/photos/deneme.jpg",
+      url: "https://example.com/magazine2",
+    },
+    {
+      id: 3,
+      title: "Discovering New Horizons in Space Exploration",
+      mainPhoto: "http://localhost:3000/img/photos/deneme.jpg",
+      url: "https://example.com/magazine3",
+    },
+    {
+      id: 4,
+      title: "Space Exploration",
+      mainPhoto: "http://localhost:3000/img/photos/deneme.jpg",
+      url: "https://example.com/magazine3",
+    },
+    // Add more entries as needed
+  ];
+  
   return (
-    <div>Magazine</div>
+    <div>
+      <div>
+      
+      <div className='left-card'>
+        <div className="title"><h1>Dergilerimiz</h1> <hr /></div>
+      </div>
+      <EventGallery events={dummyMagazineData} />
+    </div>
+    </div>
   )
 }
 
