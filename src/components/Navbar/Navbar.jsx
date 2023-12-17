6fb38055fdc8c8299c5db7d2d2a39637124e0996
iaymergen <iaymergen@hotmail.com>
2023-12-18 01:02:05 +0300

    Merge about-branch into main

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const Menu = () => (
-   <>
+  <>
     <p className='hover-animation p1'><a href="#">Ana Sayfa</a></p>
-   <p className='hover-animation p1'><a href="#">Hakkımızda</a></p>
+    <p className='hover-animation p1'><a href="#">Hakkımızda</a></p>
     <p className='hover-animation p2'><a href="#">Etkinliklerimiz</a></p>
     <p className='hover-animation p3'><a href="#">Magazin</a></p>
     <p className='hover-animation p4'><a href="#">Ekibimiz</a></p>
     <p className='hover-animation p5'><a href="#">İletişim</a></p>
-   </>
+  </>
 )
 
 const Menu1 = () => (
   <>
     <p><a href="#">Ana Sayfa</a></p>
-  <p><a href="#">Hakkımızda</a></p>
-   <p><a href="#">Etkinliklerimiz</a></p>
-   <p><a href="#">Magazin</a></p>
-   <p><a href="#">Ekibimiz</a></p>
-   <p><a href="#">İletişim</a></p>
+    <p><a href="#">Hakkımızda</a></p>
+    <p><a href="#">Etkinliklerimiz</a></p>
+    <p><a href="#">Magazin</a></p>
+    <p><a href="#">Ekibimiz</a></p>
+    <p><a href="#">İletişim</a></p>
   </>
 )
 
 const Navbar = () => {
 
   const [toggleMenu, setToggleMenu] = useState(false);
-  
+
   return (
     <div className='eestec__navbar'>
       <div className='eestec__navbar-links'>
@@ -40,17 +40,17 @@ const Navbar = () => {
         </div>
 
       </div>
-        <div className='eestec__navbar-menu'>
+      <div className='eestec__navbar-menu'>
         {toggleMenu
-        ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
-        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>
-      }
-      {toggleMenu && (
-        <div className='eestec__navbar-menu_container slide-left'>
-            <Menu1 /> 
-        </div>
-      )}
-        </div>
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='eestec__navbar-menu_container slide-left'>
+            <Menu1 />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
