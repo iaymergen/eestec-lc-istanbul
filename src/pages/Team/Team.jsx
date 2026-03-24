445ef744cb1cc8cfee849a973aec6d4da1c83031
adairmak <koyluuada@gmail.com>
2026-03-24 15:08:47 +0300

    Update Team.jsx

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -155,7 +155,7 @@ const Team = () => {
         <div className="teamtitle"><h1>Magazin Takım</h1><h1>Koordinatörleri</h1></div>
       </div>
 
-      <div className="group left">
+      <div className="group center">
         <div className="teamtitle"><h1>IGZ </h1><h1>Organizatörleri</h1></div>
         <div className='people-div'>
           {peopleData.slice(14, 16).map((person, index) => (
